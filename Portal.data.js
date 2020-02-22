fc96385fd86008e0098706c272ca57fb7664b025
Renji117 <vincent.maginot@gmail.com>
2020-02-22 01:58:11 +0100

    update

---
+++ Portal.data.js
@@ -0,0 +1,182 @@
+
+var Module = typeof Module !== 'undefined' ? Module : {};
+
+if (!Module.expectedDataFileDownloads) {
+  Module.expectedDataFileDownloads = 0;
+  Module.finishedDataFileDownloads = 0;
+}
+Module.expectedDataFileDownloads++;
+(function() {
+ var loadPackage = function(metadata) {
+
+    var PACKAGE_PATH;
+    if (typeof window === 'object') {
+      PACKAGE_PATH = window['encodeURIComponent'](window.location.pathname.toString().substring(0, window.location.pathname.toString().lastIndexOf('/')) + '/');
+    } else if (typeof location !== 'undefined') {
+      // worker
+      PACKAGE_PATH = encodeURIComponent(location.pathname.toString().substring(0, location.pathname.toString().lastIndexOf('/')) + '/');
+    } else {
+      throw 'using preloaded data can only be done on a web page or in a web worker';
+    }
+    var PACKAGE_NAME = 'C:/OneDrive/Documents/LUDUS/Unreal Projects/Portal/Binaries/HTML5/Portal.data';
+    var REMOTE_PACKAGE_BASE = 'Portal.data';
+    if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
+      Module['locateFile'] = Module['locateFilePackage'];
+      err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
+    }
+    var REMOTE_PACKAGE_NAME = Module['locateFile'] ? Module['locateFile'](REMOTE_PACKAGE_BASE, '') : REMOTE_PACKAGE_BASE;
+  
+    var REMOTE_PACKAGE_SIZE = metadata.remote_package_size;
+    var PACKAGE_UUID = metadata.package_uuid;
+  
+    function fetchRemotePackage(packageName, packageSize, callback, errback) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', packageName, true);
+      xhr.responseType = 'arraybuffer';
+      xhr.onprogress = function(event) {
+        var url = packageName;
+        var size = packageSize;
+        if (event.total) size = event.total;
+        if (event.loaded) {
+          if (!xhr.addedTotal) {
+            xhr.addedTotal = true;
+            if (!Module.dataFileDownloads) Module.dataFileDownloads = {};
+            Module.dataFileDownloads[url] = {
+              loaded: event.loaded,
+              total: size
+            };
+          } else {
+            Module.dataFileDownloads[url].loaded = event.loaded;
+          }
+          var total = 0;
+          var loaded = 0;
+          var num = 0;
+          for (var download in Module.dataFileDownloads) {
+          var data = Module.dataFileDownloads[download];
+            total += data.total;
+            loaded += data.loaded;
+            num++;
+          }
+          total = Math.ceil(total * Module.expectedDataFileDownloads/num);
+          if (Module['setStatus']) Module['setStatus']('Downloading data... (' + loaded + '/' + total + ')');
+        } else if (!Module.dataFileDownloads) {
+          if (Module['setStatus']) Module['setStatus']('Downloading data...');
+        }
+      };
+      xhr.onerror = function(event) {
+        throw new Error("NetworkError for: " + packageName);
+      }
+      xhr.onload = function(event) {
+        if (xhr.status == 200 || xhr.status == 304 || xhr.status == 206 || (xhr.status == 0 && xhr.response)) { // file URLs can return 0
+          var packageData = xhr.response;
+          callback(packageData);
+        } else {
+          throw new Error(xhr.statusText + " : " + xhr.responseURL);
+        }
+      };
+      xhr.send(null);
+    };
+
+    function handleError(error) {
+      console.error('package error:', error);
+    };
+  
+      var fetchedCallback = null;
+      var fetched = Module['getPreloadedPackage'] ? Module['getPreloadedPackage'](REMOTE_PACKAGE_NAME, REMOTE_PACKAGE_SIZE) : null;
+
+      if (!fetched) fetchRemotePackage(REMOTE_PACKAGE_NAME, REMOTE_PACKAGE_SIZE, function(data) {
+        if (fetchedCallback) {
+          fetchedCallback(data);
+          fetchedCallback = null;
+        } else {
+          fetched = data;
+        }
+      }, handleError);
+    
+  function runWithFS() {
+
+    function assert(check, msg) {
+      if (!check) throw msg + new Error().stack;
+    }
+Module['FS_createPath']('/', 'Engine', true, true);
+Module['FS_createPath']('/Engine', 'Content', true, true);
+Module['FS_createPath']('/Engine/Content', 'SlateDebug', true, true);
+Module['FS_createPath']('/Engine/Content/SlateDebug', 'Fonts', true, true);
+Module['FS_createPath']('/', 'Portal', true, true);
+Module['FS_createPath']('/Portal', 'Content', true, true);
+Module['FS_createPath']('/Portal/Content', 'Paks', true, true);
+
+    function DataRequest(start, end, audio) {
+      this.start = start;
+      this.end = end;
+      this.audio = audio;
+    }
+    DataRequest.prototype = {
+      requests: {},
+      open: function(mode, name) {
+        this.name = name;
+        this.requests[name] = this;
+        Module['addRunDependency']('fp ' + this.name);
+      },
+      send: function() {},
+      onload: function() {
+        var byteArray = this.byteArray.subarray(this.start, this.end);
+        this.finish(byteArray);
+      },
+      finish: function(byteArray) {
+        var that = this;
+
+        Module['FS_createDataFile'](this.name, null, byteArray, true, true, true); // canOwn this data in the filesystem, it is a slide into the heap that will never change
+        Module['removeRunDependency']('fp ' + that.name);
+
+        this.requests[this.name] = null;
+      }
+    };
+
+        var files = metadata.files;
+        for (var i = 0; i < files.length; ++i) {
+          new DataRequest(files[i].start, files[i].end, files[i].audio).open('GET', files[i].filename);
+        }
+
+  
+    function processPackageData(arrayBuffer) {
+      Module.finishedDataFileDownloads++;
+      assert(arrayBuffer, 'Loading data file failed.');
+      assert(arrayBuffer instanceof ArrayBuffer, 'bad input to processPackageData');
+      var byteArray = new Uint8Array(arrayBuffer);
+      var curr;
+      
+        // Reuse the bytearray from the XHR as the source for file reads.
+        DataRequest.prototype.byteArray = byteArray;
+  
+          var files = metadata.files;
+          for (var i = 0; i < files.length; ++i) {
+            DataRequest.prototype.requests[files[i].filename].onload();
+          }
+              Module['removeRunDependency']('datafile_C:/OneDrive/Documents/LUDUS/Unreal Projects/Portal/Binaries/HTML5/Portal.data');
+
+    };
+    Module['addRunDependency']('datafile_C:/OneDrive/Documents/LUDUS/Unreal Projects/Portal/Binaries/HTML5/Portal.data');
+  
+    if (!Module.preloadResults) Module.preloadResults = {};
+  
+      Module.preloadResults[PACKAGE_NAME] = {fromCache: false};
+      if (fetched) {
+        processPackageData(fetched);
+        fetched = null;
+      } else {
+        fetchedCallback = processPackageData;
+      }
+    
+  }
+  if (Module['calledRun']) {
+    runWithFS();
+  } else {
+    if (!Module['preRun']) Module['preRun'] = [];
+    Module["preRun"].push(runWithFS); // FS is not initialized yet, wait for it
+  }
+
+ }
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 146, "filename": "/Manifest_DebugFiles_HTML5.txt"}, {"start": 146, "audio": 0, "end": 199, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 199, "audio": 0, "end": 199, "filename": "/Manifest_ObsoleteNonUFSFiles_CProgram FilesMozilla Firefoxfirefox.txt"}, {"start": 199, "audio": 0, "end": 199, "filename": "/Manifest_ObsoleteUFSFiles_CProgram FilesMozilla Firefoxfirefox.txt"}, {"start": 199, "audio": 0, "end": 370, "filename": "/UE4CommandLine.txt"}, {"start": 370, "audio": 0, "end": 1300, "filename": "/Engine/Content/SlateDebug/Fonts/LastResort.tps"}, {"start": 1300, "audio": 0, "end": 5396352, "filename": "/Engine/Content/SlateDebug/Fonts/LastResort.ttf"}, {"start": 5396352, "audio": 0, "end": 1558321953, "filename": "/Portal/Content/Paks/Portal-HTML5.pak"}], "remote_package_size": 1558321953, "package_uuid": "f63ad67f-ccac-43f1-ae3a-f877aeb2e730"});
+
+})();
---
+++ index.md
@@ -0,0 +1,24 @@
+## Welcome to my Flash Game Remake project
+
+This game is a remake of Portal Flash
+
+### Gameplay
+
+You can shoot two portals and travel through them to reach unattainable locations.
+
+```markdown
+
+- Q, D : move left or right
+- Z, S : move camera forward or backward
+- Left, Right, Up, Down : shoot portal
+- Space: Jump
+- Shift : Run
+- Backspace : Restart current level
+- T : Slow motion
+
+```
+[Click here to play](https://vmaginot.github.io/Portal/Portal.html)
+
+### Support or Contact
+
+Feel free to contact me if you have any questions
